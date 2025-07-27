b71656b4c1d5d3fc6fc756566c8786896f45c02b
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-27 12:27:33 +0530

    .

--- src/main/java/org/vimal/security/v2/services/AuthenticationService.java
+++ src/main/java/org/vimal/security/v2/services/AuthenticationService.java
@@ -82,8 +82,7 @@ public class AuthenticationService {
                 shouldDoMFA = true;
             if (shouldDoMFA)
                 return Map.of("message", "MFA required", "state_token", generateStateToken(user), "mfa_methods", user.getEnabledMfaMethods());
-            var isMFAForced = unleash.isEnabled("FORCE_MFA");
-            if (isMFAForced)
+            if (unleash.isEnabled("FORCE_MFA"))
                 return Map.of("message", "MFA required. Please continue with email MFA", "state_token", generateStateToken(user));
         }
         return jwtUtility.generateTokens(user);

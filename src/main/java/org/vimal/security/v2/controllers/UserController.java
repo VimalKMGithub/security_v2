a3647e384c6997bc393d350c260583ac378c5923
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-28 00:17:09 +0530

    .

--- src/main/java/org/vimal/security/v2/controllers/UserController.java
+++ src/main/java/org/vimal/security/v2/controllers/UserController.java
@@ -1,13 +1,24 @@
 package org.vimal.security.v2.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vimal.security.v2.dtos.RegistrationDto;
 import org.vimal.security.v2.services.UserService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, Object>> register(@RequestBody RegistrationDto dto) {
+        return userService.register(dto);
+    }
 }
--- src/main/java/org/vimal/security/v2/services/UserService.java
+++ src/main/java/org/vimal/security/v2/services/UserService.java
@@ -1,9 +1,15 @@
 package org.vimal.security.v2.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.vimal.security.v2.dtos.RegistrationDto;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    public ResponseEntity<Map<String, Object>> register(RegistrationDto dto) {
+    }
 }

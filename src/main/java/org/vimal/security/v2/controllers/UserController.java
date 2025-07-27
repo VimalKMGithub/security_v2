de0b85f511254caad66dfd009ff43769a2a8d70a
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-28 03:40:21 +0530

    .

--- src/main/java/org/vimal/security/v2/controllers/UserController.java
+++ src/main/java/org/vimal/security/v2/controllers/UserController.java
@@ -1,5 +1,6 @@
 package org.vimal.security.v2.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.vimal.security.v2.dtos.RegistrationDto;
 import org.vimal.security.v2.services.UserService;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 @RestController
@@ -18,7 +25,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, Object>> register(@RequestBody RegistrationDto dto) {
+    public ResponseEntity<Map<String, Object>> register(@RequestBody RegistrationDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.register(dto);
     }
 }

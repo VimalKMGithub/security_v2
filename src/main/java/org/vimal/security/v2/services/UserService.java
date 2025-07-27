156ca787d18239e56b0cfe22f33a9233f7dbc0da
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-28 01:33:55 +0530

    .

--- src/main/java/org/vimal/security/v2/services/UserService.java
+++ src/main/java/org/vimal/security/v2/services/UserService.java
@@ -7,17 +7,24 @@ import org.springframework.stereotype.Service;
 import org.vimal.security.v2.dtos.RegistrationDto;
 import org.vimal.security.v2.enums.FeatureFlags;
 import org.vimal.security.v2.exceptions.BadRequestException;
+import org.vimal.security.v2.repos.UserRepo;
+import org.vimal.security.v2.utils.InputValidationUtility;
 
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepo userRepo;
     private final Unleash unleash;
 
     public ResponseEntity<Map<String, Object>> register(RegistrationDto dto) {
         if (unleash.isEnabled(FeatureFlags.REGISTRATION_ENABLED.name())) {
-            return ResponseEntity.ok(Map.of("test", dto));
+            var invalidInputs = InputValidationUtility.validateInputs(dto);
+            if (!invalidInputs.isEmpty())
+                return ResponseEntity.badRequest().body(Map.of("invalid_inputs", invalidInputs));
+            if (userRepo.existsByUsername(dto.username))
+                throw new BadRequestException("Username: '" + dto.username + "' is already taken");
         }
         throw new BadRequestException("Registration is currently disabled. Please try again later");
     }

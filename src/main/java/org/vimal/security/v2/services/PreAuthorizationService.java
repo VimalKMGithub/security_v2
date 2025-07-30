9d2264fe0efd79c2237ee3dce156903ab0e87a84
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-30 23:33:32 +0530

    .

--- src/main/java/org/vimal/security/v2/services/PreAuthorizationService.java
+++ src/main/java/org/vimal/security/v2/services/PreAuthorizationService.java
@@ -4,64 +4,65 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.vimal.security.v2.enums.SystemPermissions;
 import org.vimal.security.v2.enums.SystemRoles;
-import org.vimal.security.v2.utils.ArrayUtility;
 import org.vimal.security.v2.utils.UserUtility;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service("PreAuth")
 public class PreAuthorizationService {
-    private static final String[] TOP_ROLES_ARRAY = SystemRoles.TOP_ROLES.toArray(new String[0]);
-    private static final String[] CAN_CREATE_USERS_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_CREATE_USER.name());
-    private static final String[] CAN_READ_USERS_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_READ_USER.name());
-    private static final String[] CAN_UPDATE_USERS_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_UPDATE_USER.name());
-    private static final String[] CAN_DELETE_USERS_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_DELETE_USER.name());
-    private static final String[] CAN_READ_PERMISSIONS_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_READ_PERMISSION.name());
-    private static final String[] CAN_CREATE_ROLES_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_CREATE_ROLE.name());
-    private static final String[] CAN_READ_ROLES_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_READ_ROLE.name());
-    private static final String[] CAN_UPDATE_ROLES_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_UPDATE_ROLE.name());
-    private static final String[] CAN_DELETE_ROLES_ARRAY = ArrayUtility.addGivenStringToGivenStringArray(TOP_ROLES_ARRAY, SystemPermissions.CAN_DELETE_ROLE.name());
+    private static final Collection<String> TOP_ROLES_COLLECTION = Set.copyOf(SystemRoles.TOP_ROLES);
+    private static final Collection<String> CAN_CREATE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_CREATE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_READ_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_UPDATE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_UPDATE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_DELETE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_DELETE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_READ_PERMISSIONS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_PERMISSION.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_CREATE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_CREATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_READ_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_UPDATE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_UPDATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Collection<String> CAN_DELETE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_DELETE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
 
     public boolean canCreateUsers() {
-        return hasAnyAuthority(CAN_CREATE_USERS_ARRAY);
+        return hasAnyAuthority(CAN_CREATE_USERS_COLLECTION);
     }
 
     public boolean canReadUsers() {
-        return hasAnyAuthority(CAN_READ_USERS_ARRAY);
+        return hasAnyAuthority(CAN_READ_USERS_COLLECTION);
     }
 
     public boolean canUpdateUsers() {
-        return hasAnyAuthority(CAN_UPDATE_USERS_ARRAY);
+        return hasAnyAuthority(CAN_UPDATE_USERS_COLLECTION);
     }
 
     public boolean canDeleteUsers() {
-        return hasAnyAuthority(CAN_DELETE_USERS_ARRAY);
+        return hasAnyAuthority(CAN_DELETE_USERS_COLLECTION);
     }
 
     public boolean canReadPermissions() {
-        return hasAnyAuthority(CAN_READ_PERMISSIONS_ARRAY);
+        return hasAnyAuthority(CAN_READ_PERMISSIONS_COLLECTION);
     }
 
     public boolean canCreateRoles() {
-        return hasAnyAuthority(CAN_CREATE_ROLES_ARRAY);
+        return hasAnyAuthority(CAN_CREATE_ROLES_COLLECTION);
     }
 
     public boolean canReadRoles() {
-        return hasAnyAuthority(CAN_READ_ROLES_ARRAY);
+        return hasAnyAuthority(CAN_READ_ROLES_COLLECTION);
     }
 
     public boolean canUpdateRoles() {
-        return hasAnyAuthority(CAN_UPDATE_ROLES_ARRAY);
+        return hasAnyAuthority(CAN_UPDATE_ROLES_COLLECTION);
     }
 
     public boolean canDeleteRoles() {
-        return hasAnyAuthority(CAN_DELETE_ROLES_ARRAY);
+        return hasAnyAuthority(CAN_DELETE_ROLES_COLLECTION);
     }
 
-    public boolean hasAnyAuthority(String... authorities) {
-        var requiredRoles = Set.of(authorities);
+    public boolean hasAnyAuthority(Collection<String> authorities) {
         return UserUtility.getAuthenticationOfCurrentAuthenticatedUser().getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .anyMatch(requiredRoles::contains);
+                .anyMatch(authorities::contains);
     }
 }

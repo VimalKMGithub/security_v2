f5524be29a58d45299ef97ae12ba0bdb6779e315
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-31 00:07:55 +0530

    .

--- src/main/java/org/vimal/security/v2/services/AdminService.java
+++ src/main/java/org/vimal/security/v2/services/AdminService.java
@@ -184,7 +184,7 @@ public class AdminService {
             var ownAccountFoundWithUsernameOrEmail = new HashSet<String>();
             if (usernames.contains(user.getUsername())) ownAccountFoundWithUsernameOrEmail.add(user.getUsername());
             if (emails.contains(user.getUserModel().getEmail()))
-                ownAccountFoundWithUsernameOrEmail.add(user.getEmail());
+                ownAccountFoundWithUsernameOrEmail.add(user.getUserModel().getEmail());
             if (!ownAccountFoundWithUsernameOrEmail.isEmpty())
                 mapOfErrors.put("you_cannot_delete_your_own_account_using_this_endpoint", ownAccountFoundWithUsernameOrEmail);
             if (!mapOfErrors.isEmpty()) return ResponseEntity.badRequest().body(mapOfErrors);

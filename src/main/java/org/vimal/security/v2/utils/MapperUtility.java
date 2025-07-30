5c77e377595901ddc97ff44be954f3cea32929e2
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-07-30 14:01:16 +0530

    .

--- src/main/java/org/vimal/security/v2/utils/MapperUtility.java
+++ src/main/java/org/vimal/security/v2/utils/MapperUtility.java
@@ -10,32 +10,21 @@ import java.util.stream.Collectors;
 public class MapperUtility {
     public static UserSummaryDto toUserSummaryDto(UserModel user) {
         var dto = new UserSummaryDto();
-        dto.setId(user.getId());
-        dto.setFirstName(user.getFirstName());
-        dto.setMiddleName(user.getMiddleName());
-        dto.setLastName(user.getLastName());
-        dto.setUsername(user.getUsername());
-        dto.setEmail(user.getEmail());
-        dto.setCreatedBy(user.getCreatedBy());
-        dto.setUpdatedBy(user.getUpdatedBy());
-        dto.setRoles(user.getRoles().stream().map(RoleModel::getRoleName).collect(Collectors.toSet()));
-        dto.setMfaMethods(user.getEnabledMfaMethods().stream().map(UserModel.MfaType::name).collect(Collectors.toSet()));
-        dto.setLastLoginAt(user.getLastLoginAt());
-        dto.setPasswordChangedAt(user.getPasswordChangedAt());
-        dto.setCreatedAt(user.getCreatedAt());
-        dto.setUpdatedAt(user.getUpdatedAt());
-        dto.setLastLockedAt(user.getLastLockedAt());
-        dto.setEmailVerified(user.isEmailVerified());
-        dto.setMfaEnabled(user.isMfaEnabled());
-        dto.setAccountLocked(user.isAccountLocked());
-        dto.setAccountEnabled(user.isAccountEnabled());
-        dto.setFailedLoginAttempts(user.getFailedLoginAttempts());
-        dto.setFailedMfaAttempts(user.getFailedMfaAttempts());
+        mapCommonFields(user, dto);
         return dto;
     }
 
     public static UserSummaryToCompanyUsersDto toUserSummaryToCompanyUsersDto(UserModel user) {
         var dto = new UserSummaryToCompanyUsersDto();
+        mapCommonFields(user, dto);
+        dto.setAccountDeleted(user.isAccountDeleted());
+        dto.setAccountDeletedAt(user.getAccountDeletedAt());
+        dto.setDeletedBy(user.getDeletedBy());
+        return dto;
+    }
+
+    public static void mapCommonFields(UserModel user,
+                                       UserSummaryDto dto) {
         dto.setId(user.getId());
         dto.setFirstName(user.getFirstName());
         dto.setMiddleName(user.getMiddleName());
@@ -55,11 +44,7 @@ public class MapperUtility {
         dto.setMfaEnabled(user.isMfaEnabled());
         dto.setAccountLocked(user.isAccountLocked());
         dto.setAccountEnabled(user.isAccountEnabled());
-        dto.setAccountDeleted(user.isAccountDeleted());
-        dto.setAccountDeletedAt(user.getAccountDeletedAt());
-        dto.setDeletedBy(user.getDeletedBy());
         dto.setFailedLoginAttempts(user.getFailedLoginAttempts());
         dto.setFailedMfaAttempts(user.getFailedMfaAttempts());
-        return dto;
     }
 }

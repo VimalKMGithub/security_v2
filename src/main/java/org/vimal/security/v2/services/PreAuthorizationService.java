8f68acb7e26025617bb2931baded298cc523ea09
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-08-07 23:07:19 +0530

    .

--- src/main/java/org/vimal/security/v2/services/PreAuthorizationService.java
+++ src/main/java/org/vimal/security/v2/services/PreAuthorizationService.java
@@ -12,51 +12,51 @@ import java.util.stream.Stream;
 
 @Service("PreAuth")
 public class PreAuthorizationService {
-    private static final Set<String> TOP_ROLES_COLLECTION = Set.copyOf(SystemRoles.TOP_ROLES);
-    private static final Set<String> CAN_CREATE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_CREATE_USER.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_READ_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_USER.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_UPDATE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_UPDATE_USER.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_DELETE_USERS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_DELETE_USER.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_READ_PERMISSIONS_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_PERMISSION.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_CREATE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_CREATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_READ_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_READ_ROLE.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_UPDATE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_UPDATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
-    private static final Set<String> CAN_DELETE_ROLES_COLLECTION = Stream.concat(TOP_ROLES_COLLECTION.stream(), Stream.of(SystemPermissions.CAN_DELETE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> TOP_ROLES_SET = Set.copyOf(SystemRoles.TOP_ROLES);
+    private static final Set<String> CAN_CREATE_USERS_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_CREATE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_READ_USERS_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_READ_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_UPDATE_USERS_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_UPDATE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_DELETE_USERS_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_DELETE_USER.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_READ_PERMISSIONS_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_READ_PERMISSION.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_CREATE_ROLES_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_CREATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_READ_ROLES_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_READ_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_UPDATE_ROLES_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_UPDATE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
+    private static final Set<String> CAN_DELETE_ROLES_SET = Stream.concat(TOP_ROLES_SET.stream(), Stream.of(SystemPermissions.CAN_DELETE_ROLE.name())).collect(Collectors.toUnmodifiableSet());
 
     public boolean canCreateUsers() {
-        return hasAnyAuthority(CAN_CREATE_USERS_COLLECTION);
+        return hasAnyAuthority(CAN_CREATE_USERS_SET);
     }
 
     public boolean canReadUsers() {
-        return hasAnyAuthority(CAN_READ_USERS_COLLECTION);
+        return hasAnyAuthority(CAN_READ_USERS_SET);
     }
 
     public boolean canUpdateUsers() {
-        return hasAnyAuthority(CAN_UPDATE_USERS_COLLECTION);
+        return hasAnyAuthority(CAN_UPDATE_USERS_SET);
     }
 
     public boolean canDeleteUsers() {
-        return hasAnyAuthority(CAN_DELETE_USERS_COLLECTION);
+        return hasAnyAuthority(CAN_DELETE_USERS_SET);
     }
 
     public boolean canReadPermissions() {
-        return hasAnyAuthority(CAN_READ_PERMISSIONS_COLLECTION);
+        return hasAnyAuthority(CAN_READ_PERMISSIONS_SET);
     }
 
     public boolean canCreateRoles() {
-        return hasAnyAuthority(CAN_CREATE_ROLES_COLLECTION);
+        return hasAnyAuthority(CAN_CREATE_ROLES_SET);
     }
 
     public boolean canReadRoles() {
-        return hasAnyAuthority(CAN_READ_ROLES_COLLECTION);
+        return hasAnyAuthority(CAN_READ_ROLES_SET);
     }
 
     public boolean canUpdateRoles() {
-        return hasAnyAuthority(CAN_UPDATE_ROLES_COLLECTION);
+        return hasAnyAuthority(CAN_UPDATE_ROLES_SET);
     }
 
     public boolean canDeleteRoles() {
-        return hasAnyAuthority(CAN_DELETE_ROLES_COLLECTION);
+        return hasAnyAuthority(CAN_DELETE_ROLES_SET);
     }
 
     public boolean hasAnyAuthority(Set<String> authorities) {

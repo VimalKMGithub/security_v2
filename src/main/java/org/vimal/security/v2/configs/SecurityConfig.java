1c58a974ac9580b3eed2c29430ad4a2e35ba8cc3
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-08-13 19:20:54 +0530

    .

--- src/main/java/org/vimal/security/v2/configs/SecurityConfig.java
+++ src/main/java/org/vimal/security/v2/configs/SecurityConfig.java
@@ -65,8 +65,7 @@ public class SecurityConfig {
                         .httpStrictTransportSecurity(hsts -> hsts.includeSubDomains(true).preload(true).maxAgeInSeconds(63072000))
                         .xssProtection(xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
                         .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
-                        .contentTypeOptions(HeadersConfigurer.ContentTypeOptionsConfig::disable)
-                )
+                        .contentTypeOptions(HeadersConfigurer.ContentTypeOptionsConfig::disable))
                 .addFilterBefore(isServerUpFilterUtility, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtFilterUtility, UsernamePasswordAuthenticationFilter.class);
         return http.build();
